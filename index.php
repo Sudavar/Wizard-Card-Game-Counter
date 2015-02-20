e730f6635f848352d5597d657038856ec3480e2f
Yury Kherimyan <yury@codescar.eu>
2015-02-20 14:15:11 +0200

    Some mods and a README

--- index.php
+++ index.php
@@ -1,108 +1,91 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="style2.css" />
+        <title>Counter for Card Game Wizard</title>
+        <meta name="keywords" content="HTML5, CSS3, Jquery, Wizard Game, Card Game">
+        <meta name="description" content="Counter for Card Game Wizard">
+        <meta name="author" content="Sudavar">
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css" />
+        <link rel="stylesheet" type="text/css" href="style.css" />
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-        <script src="js.js" type="text/javascript"></script>
-        <meta charset="UTF-8"> 
+        <script src="js/bootstrap.min.js"></script>
+        <script src="game-counter.js"></script>
     </head>
     <body>
-<?php
-if ( !isset($_POST[ 'game' ]) ):
-?>
-<form method="post">
-    <fieldset class="basic-grey">
-        <h1>Wizard Game Settings</h1>
-        <label>
-            <span>Number of Players: </span>
-            <span>3</span>
-                <input id="players_number" name="players_number" type="range" min="3" max="10" step="1" value="3" />
-            <span>10</span>
-            <input type="hidden" name="game" value="Go">
-            <input type="submit" value="Start Game">
-        </label>
-    </fieldset>
-    <fieldset class="basic-grey" id="players_names">
-        <label class="pl_name">
-            <span>Name 1:</span>
-            <input type="text" name="player[]" />
-        </label>
-        <label class="pl_name">
-            <span>Name 2:</span>
-            <input type="text" name="player[]" />
-        </label>
-        <label class="pl_name">
-            <span>Name 3:</span>
-            <input type="text" name="player[]" />
-        </label>
-    </fieldset>
-</form>
-<?php
-else:
-    $players = $_POST[ 'players_number' ];
-    $rounds = 60 / $players;
-    $player_names = $_POST[ 'player' ];
-    echo "<div id=\"score-table\">
-			<div class=\"row\">
-				<div class=\"inline heading\">Game Round <span id=\"cur_round\">1</span>/$rounds</div>";
-    for ($j = 0; $j < $players; $j++) {
-        $player_name = $player_names[$j];
-        echo "<div class=\"inline heading pls\">$player_name</div>";
-    }
-    echo "</div><div id=\"holder\">";
-    for ($i = 1; $i <= $rounds; $i++) {
-        if ($i == 1)
-            $display = "";
-        else
-            $display = "hidden";
-        echo "<div class=\"row round$i $display\">";
-        echo "<div class=\"inline box\">
-				<div class=\"previous inline\">&lt;</div><div class=\"inline\"> $i </div><div class=\"next inline\">&gt;</div><br />
-				<div class=\"inline error-box\"><input type=\"button\" class=\"start_round round$i\" value=\"Start\" /></div>
-				<div class=\"inline error-box\"><div class=\"hidden message round$i\"></div></div>
-			  </div>";
-        for ($j = 1; $j <= $players; $j++) {
-            ?>
-            <div class="inline box">
-                <div class="box-buz">
-                    <span>Prediction: </span>
-                    <input  type="number" name="predicted" min="0" max="<?php echo $i; ?>" value="0"
-                            class="predicted player<?php echo $j; ?> round<?php echo $i; ?>" />
+        <div class="jumbotron"><div class="container">
+            <h1>Wizard Card Game Counter</h1>
+            <p>Some info about the application.</p>
+            <form class="form-horizontal" id="form"  autocomplete="on">
+                <div class="form-group">
+                    <div class="col-xs-3">
+                        <button type="button" class="btn btn-default reset_form">Reset</button>
+                    </div>
+                    <div class="col-xs-3">
+                        <button type="submit" class="btn btn-default">Start Game</button>
+                    </div>
+                    <div class="col-xs-offset-2 col-xs-4 col-sm-offset-3 col-sm-3">
+                        <button type="button" class="btn btn-default add_player">+</button>
+                        <button type="button" class="btn btn-default remove_player">-</button>
+                    </div>
                 </div>
-                <div class="box-buz">
-                    <span>Achieved: </span>
-                    <input  type="number" name="achieved" min="0" max="<?php echo $i; ?>" disabled="disabled" value="0"
-                            data-player-id="<?php echo $j; ?>" data-round-num="<?php echo $i; ?>"
-                            class="achieved player<?php echo $j; ?> round<?php echo $i; ?>" />
+                <div class="player_names"><div class="form-group">
+                    <label for="name_1" class="col-xs-4 control-label">Name 1:</label>
+                    <div class="col-xs-6">
+                        <input type="text" class="pl_name form-control" id="name_1" name="name_1" placeholder="" required="required" >
+                    </div>
                 </div>
-                <div class="box-buz">
-                    <span>Result: </span>
-                    <div class="inline result player<?php echo $j; ?> round<?php echo $i; ?>"></div>
+                <div class="form-group">
+                    <label for="name_2" class="col-xs-4 control-label">Name 2:</label>
+                    <div class="col-xs-6">
+                        <input type="text" class="pl_name form-control" id="name_2" name="name_2" placeholder="" required="required" >
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label for="name_3" class="col-xs-4 control-label">Name 3:</label>
+                    <div class="col-xs-6">
+                        <input type="text" class="pl_name form-control" id="name_3" name="name_3" placeholder="" required="required" >
+                    </div>
+                </div></div>
+            </form>
+        </div></div>
+        <div class="content container-fluid wrapper" style="display: none;">
+            <div class="row">
+                <div class="col-lg-1"></div>
+                <div class="col-sm-9 col-lg-8" id="main_panel" >
+                    <div class="main_info"></div>
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <span class="glyphicon glyphicon-chevron-left clickable previous_player" aria-hidden="true"></span>
+                        </div>
+                        <div class="col-xs-6">
+                            <span class="glyphicon glyphicon-chevron-right clickable next_player" aria-hidden="true"></span>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-5 col-sm-6">
+                            <div class="col-sm-5"><h4>ROUND</h4></div>
+                            <div class="col-sm-7"><h4><span class="current_round"></span> / 20</h4></div>
+                        </div>
+                        <div class="col-xs-7 col-sm-6">
+                            <div class="col-xs-6 col-sm-4">
+                                <input type="button" class="btn btn-primary disabled" value="&larr;" />
+                            </div>
+                            <div class="col-xs-6 col-sm-4">
+                                <input type="button" class="btn btn-primary disabled" value="&rarr;" />
+                            </div>
+                            <div class="col-xs-12 col-sm-4">
+                                <input type="button" class="btn btn-primary " value="Start" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3 col-lg-3 all_players" id="left_panel">
+                    <h2>Players List</h3>
+                    <div class="list-group"></div>
                 </div>
             </div>
-            <?php
-        }
-        echo "</div>";
-    }
-    echo "</div><div class=\"row\"><div class=\"inline heading\"></div>";
-    for ($j = 1; $j <= $players; $j++) {
-        ?>
-            <div class="inline heading">
-                <span>Score: </span>
-                <div class="inline score player<?php echo $j; ?>"></div>
-            </div>
-        <?php
-    }
-    echo "</div>";
-?>
-</div>
-</div>
-<style type="text/css">
-    .box{ width: <?php echo (100/($players+1)-1); ?>%; }
-    .heading{ width: <?php echo (100/($players+1)-1); ?>%; }
-</style>
-<?php
-endif;
-?>
+        </div>
     </body>
 </html>
